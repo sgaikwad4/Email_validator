65ed5d74dc93ef589603257990ddb2bace2d63dc
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-14 11:12:38 -0800

    Made function

--- email_validator.py
+++ email_validator.py
@@ -1,9 +1,12 @@
 # TASK: Write a function that validates an email format.
 
 # Define a function that takes a string parameter
+def email_validator():
+    print("hello")
 # Check if '@' is in the email
 # Extract the last four characters of the email
 # Check if it ends with '.com' or '.net'
 # Return True if valid, False otherwise
 # Prompt the user for an email input and call the function
 # Display whether the email is valid
+email_validator() 
